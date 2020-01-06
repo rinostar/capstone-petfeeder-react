60fc0053156944a6841f1458dfa530f21b227abd
Rinostar <codingrinostar@gmail.com>
2020-01-06 12:38:24 -0800

    setup template react UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Navbar,
+  Button,
+  Container
+} from 'react-bootstrap';
+import panda from './panda_icon.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar collapseOnSelect expand="lg" bg="dark">
+        <Navbar.Brand href="/" className="text-light">
+          <img 
+            src={panda}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="FoodieBear Logo"
+          />
+          &nbsp; FoodieBear PetFeeder
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+      </Navbar>
+  
+      <Container className="container-full">
+        <Button variant="primary">Feed Now</Button>
+      </Container>
+      
+      <footer>
+        <p className="text-light">@FoodieBear</p>
+      </footer>
     </div>
   );
 }
